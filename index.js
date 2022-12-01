82831e9280ad9d2de7f9b15cb71cd6009299bd47
MahadiMunna <mahadimunna.official@gmail.com>
2022-12-01 11:58:22 +0600

    writers list api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -11,6 +12,27 @@ app.use(express.json());
 app.get('/',async(req,res)=>{
     res.send('Alter books server is running')
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vivqooe.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+    try{
+        const writerCollection = client.db('alterBooks').collection('writers');
+
+        app.get('/writers',async(req,res)=>{
+            const query = {};
+            const writers = await writerCollection.find(query).toArray();
+            res.send(writers);
+        })
+
+    }finally{
+
+    }
+}
+run().catch(console.log)
+
+
 
 
 app.listen(port,()=>console.log(`Alter books serve running on ${port}`))
